ac6c45d5f2df848df04115bad9cbac119e82a1f8
Nishi Lama <123444093+NishiLama@users.noreply.github.com>
2025-10-26 21:28:38 +0530

    Create EmployeeRepository interface for JPA access

---
+++ EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.dailyrecord.repository;
+
+import com.example.dailyrecord.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
